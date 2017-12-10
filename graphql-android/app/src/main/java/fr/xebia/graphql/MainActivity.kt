f3e16d165c3dd7f888b3bf639943441b09a3de08
Benjamin Lacroix <benjlacroix@gmail.com>
2017-12-10 19:05:37 +0100

    Add Apollo example

--- graphql-android/app/src/main/java/fr/xebia/graphql/MainActivity.kt
+++ graphql-android/app/src/main/java/fr/xebia/graphql/MainActivity.kt
@@ -1,7 +1,12 @@
 package fr.xebia.graphql
 
+import GetUserQuery
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import com.apollographql.apollo.ApolloCall
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.exception.ApolloException
 import com.squareup.moshi.Moshi
 import kotlinx.android.synthetic.main.activity_main.*
 import me.lazmaid.kraph.Kraph
@@ -18,17 +23,36 @@ class MainActivity : AppCompatActivity() {
         val CLIENT = OkHttpClient()
         private val MOSHI = Moshi.Builder().build()
         private val USER_ADAPTER = MOSHI.adapter(Fake::class.java)
+        private val APOLLO = ApolloClient.builder().serverUrl(URL).okHttpClient(CLIENT).build()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Vanilla
         val q = getQueryVanilla()
-        executeAsyncHttpPost(q)
+        executeAsyncHttpPost(q, idVanillaView, firstNameVanillaView, lastNameVanillaView)
 
+        // Kraph
         val q1 = getQueryKraph()
-        executeAsyncHttpPost(q1)
+        executeAsyncHttpPost(q1, idKraphView, firstNameKraphView, lastNameKraphView)
+
+        // Apollo
+        APOLLO.query(GetUserQuery.builder().build())
+                .enqueue(object : ApolloCall.Callback<GetUserQuery.Data>() {
+                    override fun onFailure(ignored: ApolloException) {
+                        // Oops
+                    }
+
+                    override fun onResponse(response: com.apollographql.apollo.api.Response<GetUserQuery.Data>) {
+                        runOnUiThread({
+                            idApolloView.text = response.data()?.Fake()?.id()
+                            firstNameApolloView.text = response.data()?.Fake()?.firstName()
+                            lastNameApolloView.text = response.data()?.Fake()?.lastName()
+                        })
+                    }
+                })
     }
 
     private fun getQueryVanilla(): String = "{\"query\":\"{Fake{id,firstName,lastName}}\"}"
@@ -43,7 +67,7 @@ class MainActivity : AppCompatActivity() {
         }
     }.toRequestString()
 
-    private fun executeAsyncHttpPost(query: String) {
+    private fun executeAsyncHttpPost(query: String, idView: TextView, firstNameView: TextView, lastNameView: TextView) {
         val request = Request.Builder()
                 .url(URL)
                 .post(RequestBody.create(MediaType.parse("application/json; charset=utf-8"), query))
